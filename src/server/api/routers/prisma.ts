a74787883fe014f9a01e7354ea65d8d460c73340
LyghtCode <xikago.musica@gmail.com>
2023-03-13 11:23:21 -0600

    Create prisma.ts

---
+++ src/server/api/routers/prisma.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const prismaRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    return ctx.prisma.wesetproperties_1155_test.findMany({
+      orderBy: {
+        created_at: "desc",
+      },
+    });
+  }),
+  getPropsUser: publicProcedure
+    .input(z.object({ user: z.string() }))
+    .query(({ ctx, input }) => {
+      const deployer = input.user;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      return ctx.prisma.wesetproperties_1155_test.findMany({
+        where: {
+          deployer: deployer,
+        },
+      });
+    }),
+});
